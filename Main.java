fe9b2a97f430bb43ec5da643bc7d3f6242735e3d
hcs <hannahemcs@gmail.com>
2023-10-09 17:49:27 +0200

    Main method

--- Main.java
+++ Main.java
@@ -1,10 +1,13 @@
+
 import java.util.ArrayList;
 import java.util.List;
 public class Main {
     public static void main(String[] args) {
         List<Item> items = new ArrayList<>();
+
         Item item = new Item (001,"Example", 4);
         Item item2 = new Item (002,"Example Two", 0);
+
         items.add(item);
         items.add(item2);
     }
