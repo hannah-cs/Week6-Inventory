f6f1132465fcef07ecf0dda92695e067fb3fe93b
hcs <hannahemcs@gmail.com>
2023-10-09 17:16:39 +0200

    Add main class

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Item item = new Item (001,"Example", 4);
+        Item item2 = new Item (002,"Example Two", 0);
+
+    }
+}
