68ac8b198cc0cd3c0e1529f482c450e541cf5a1c
alinaberlin <alina.ghetler@gmail.com>
2023-10-09 17:28:55 +0200

    Add Inventory

--- Item.java
+++ Item.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Item {
     int id;
     String name;
@@ -34,5 +36,16 @@ public class Item {
         this.quantity = quantity;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Item item = (Item) o;
+        return getId() == item.getId();
+    }
 
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
 }
